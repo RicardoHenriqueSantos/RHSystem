31469a3527a7133c8414a5c05c4aba1bfe73ad48
RicardoHenriqueSantos <ricardo_santins@live.com>
2022-09-17 10:39:34 -0400

    RHSystem - Arquivos Iniciais

---
+++ back.py
@@ -0,0 +1,114 @@
+import mysql.connector
+import datetime
+
+
+# CONEXÃO COM O BANCO DE DADOS
+cnx = mysql.connector.connect(
+    host='localhost',
+    database='db_vendas',
+    user='root',
+    password='Kal-El52$')
+
+# INFORMAÇÃO SOBRE A CONEXÃO
+if cnx.is_connected():
+    info = cnx.get_server_info()
+    print(f"Conectado ao servidor MySQL versão {info}")
+
+# CURSOR PARA REALIZAR AS OPERAÇÕES NO PYTHON
+cursor = cnx.cursor()
+
+
+def realizar_venda(data, funcionario, produto, quantidade):
+    # SELECIONA O PRODUTO ESCOLHIDO PELO USUÁRIO
+    valor_produto = cursor.execute(f"select prod_venda from produto where prod_cod = {produto}")
+    valor_produto = cursor.fetchall()
+
+    # MULTIPLICA PELA QUANTIDADE INSERIDA
+    preco_total = (valor_produto[0][0]) * quantidade
+
+    # REDUZ A QUANTIDADE DO ESTOQUE
+    estoque = cursor.execute(f"select prod_estoque from produto where prod_cod = {produto}")
+    estoque = cursor.fetchall()
+    atualizacao = (estoque[0][0]) - quantidade
+
+    # INSERÇÃO DA NOVA VENDA COM REDUÇÃO DO ESTOQUE
+    cursor.execute(f"update produto set prod_estoque = {atualizacao} where prod_cod = {produto}")
+    cursor.execute(f"insert into venda ("
+                   f"ven_data,"
+                   f"ven_total,"
+                   f"ven_cod_fun,"
+                   f"ven_cod_produto,"
+                   f"ven_quant) values ("
+                   f"'{data}', "
+                   f"{preco_total}, "
+                   f"{funcionario}, "
+                   f"{produto}, "
+                   f"{quantidade})")
+    # REALIZAR TODAS OPERAÇÕES NO BANCO DE DADOS
+    cnx.commit()
+
+# TESTE PARA REALIZAR VENDA
+    # realizar_venda('13/09/2022', 20220902, 780, 1)
+
+
+def filtrar_venda(funcionario):
+    minhas_vendas = cursor.execute(f"select ven_data, "
+                                   f"ven_total from venda "
+                                   f"where ven_cod_fun = {funcionario}")
+    minhas_vendas = cursor.fetchall()
+
+    for dados in minhas_vendas:
+        print(dados)
+
+    cnx.commit()
+
+    return minhas_vendas
+
+# TESTE VENDA FILTRADA
+    # filtrar_venda(20220902)
+
+
+def consultar_produtos():
+    produtos = cursor.execute("select prod_cod, prod_item, prod_venda from produto where prod_estoque > 0")
+    produtos = cursor.fetchall()
+
+    for dados_produto in produtos:
+        print(dados_produto)
+    cnx.commit()
+    return produtos
+
+# TESTE PARA CONSULTAR PRODUTOS
+    # consultar_produtos()
+
+
+def cadastrar_produtos(item, marca, custo, venda, tipo, estoque):
+    produtos = cursor.execute(f"insert into produto ("
+                              f"prod_item, "
+                              f"prod_marca, "
+                              f"prod_custo, "
+                              f"prod_venda, "
+                              f"prod_tipo, "
+                              f"prod_estoque) values "
+                              f"('{item}', "
+                              f"'{marca}', "
+                              f"{custo}, "
+                              f"{venda}, "
+                              f"'{tipo}', "
+                              f"{estoque})")
+    produtos = cursor.fetchall()
+
+    for dados_produto in produtos:
+        print(dados_produto)
+
+    cnx.commit()
+
+
+# cadastrar_produtos('Camiseta Eagle Fly Free', 'White Metal', 42.50, 89.90, 'Camiseta', 9)
+# consultar_produtos()
+
+
+def data_atual():
+    DataAtual = datetime.date.today()
+    data = DataAtual.strftime("%d/%m/%Y")
+    print(data)
+    return data
---
+++ main.py
@@ -0,0 +1,261 @@
+import mysql.connector
+import PySimpleGUI as sg
+import back
+from webbrowser import open
+
+gitLink = 'https://app.powerbi.com/view?r' \
+          '=eyJrIjoiNzc4MjFhOTYtOGI4OC00MDhhL' \
+          'ThkYWEtMDE5YTgyNzU3YzIyIiwidCI6ImZhNGNk' \
+          'YjlhLThkMDQtNDJmMy1iODMwLWU0N2I3YmFmMTdmZiIs' \
+          'ImMiOjF9&pageName=ReportSectiona39f44bfe132f21b7b7a '
+sg.theme('DarkBlue14')
+
+
+def front():
+    global fun_cod
+    # LAYOUT DA JANELA INICIAL
+    flayout = [
+        [sg.Text('')],
+        [sg.Text('Usuario'), sg.Input(key='usuario', size=(20, 1))],
+        [sg.Text('Senha  '), sg.Input(key='senha', password_char='*', size=(20, 1))],
+        [sg.Text('')],
+        [sg.Text('                '), sg.Button('Entrar', size=(6, 2))]
+    ]
+
+    # ATRIBUINDO E CONFIGURANDO A JANELA
+    janela = sg.Window('Página Inicial', flayout, size=(239, 150))
+
+    # VERIFICAÇÃO LOGIN E SENHA
+    while True:
+        # CONEXÃO COM O BANCO DE DADOS
+        cnx = mysql.connector.connect(
+            host='localhost',
+            database='db_vendas',
+            user='root',
+            password='Kal-El52$')
+
+        # INFORMAÇÃO SOBRE A CONEXÃO
+        if cnx.is_connected():
+            info = cnx.get_server_info()
+            print(f"Conectado ao servidor MySQL versão {info}")
+
+        # CURSOR PARA REALIZAR AS OPERAÇÕES NO PYTHON
+        cursor = cnx.cursor()
+        evento, valores = janela.read()
+
+        if evento == sg.WINDOW_CLOSED:
+            break
+
+        if evento == 'Entrar':
+            usuariopy = int(valores['usuario'])
+            senhapy = int(valores['senha'])
+
+            cursor.execute(f"select fun_cod from funcionario where fun_cod = {usuariopy}")
+            usuariosql = cursor.fetchall()
+
+            # VERIFICAÇÃO DO CÓDIGO DO USUÁRIO
+            if not usuariosql:
+                print("Usuário Não Encontrado!")
+
+            # VERIFICAÇÃO DA SENHA DO USUÁRIO
+            elif usuariopy in usuariosql[0]:
+                cursor.execute(f"select fun_senha from funcionario where fun_cod= {usuariopy}")
+                senhasql = cursor.fetchall()
+                fun_cod = usuariopy
+                if senhapy in senhasql[0]:
+                    print("Login Realizado Com Sucesso!")
+                    janela.close()
+                    selecao()
+                else:
+                    print("Senha Inválida!")
+
+
+            cnx.commit()
+            cnx.close()
+
+
+def selecao():
+    # PÁGINA PARA SELEÇÃO DOS SETORES
+    pagina_selecao = [
+        [sg.Text(' ')],
+        [sg.Text('Selecione o Setor')],
+        [sg.Text(' ')],
+        [sg.Button('Vendas', size=(8, 3)), sg.Button('Produtos', size=(8, 3))],
+        [sg.Text(' ')],
+        [sg.Text('Acessar Relatório', enable_events=True, key='Link'), sg.Text('|'), sg.Text('Finalizar Acesso', enable_events=True, key='Sair')],
+    ]
+
+    # ATRIBUIÇÃO DOS EVENTOS E VALORES
+    pagina_2 = sg.Window('Setores', pagina_selecao, size=(300, 210), element_justification='center')
+    evento_pag_2, valor_pag_2 = pagina_2.read()
+
+    # EXECUÇÃO DA JANELA E AÇÕES
+    while True:
+
+        if evento_pag_2 == sg.WINDOW_CLOSED:
+            pagina_2.close()
+
+        if evento_pag_2 == 'Sair':
+            pagina_2.close()
+
+        if evento_pag_2 == 'Link':
+            open(gitLink)
+            break
+
+        if evento_pag_2 == 'Vendas':
+            back.filtrar_venda(fun_cod)
+            pagina_2.close()
+            area_vendas()
+
+        if evento_pag_2 == 'Produtos':
+            pagina_2.close()
+            area_produtos()
+
+
+def area_vendas():
+    minhas_vendas = back.filtrar_venda(fun_cod)
+    area_venda = [
+        [sg.Text('Minhas Vendas')],
+        [sg.Listbox(minhas_vendas, size=(50, 10), key='box')],
+        [sg.Text('')],
+        [sg.Button('Voltar', size=(14, 2)), sg.Button('Realizar Venda', size=(14, 2))]
+    ]
+    pagina_3 = sg.Window('Área de Vendas', area_venda, size=(276, 310))
+    evento_pag_3, valor_pag_3 = pagina_3.read()
+
+    while True:
+
+        if evento_pag_3 == sg.WINDOW_CLOSED:
+            pagina_3.close()
+
+        if evento_pag_3 == 'Voltar':
+            pagina_3.close()
+            selecao()
+
+        if evento_pag_3 == 'Realizar Venda':
+            pagina_3.close()
+            realizar_venda()
+
+
+def area_produtos():
+    lista_produtos = back.consultar_produtos()
+    # print(f"{lista_produtos[0][0]} | {lista_produtos[0][1]} | R$ {lista_produtos[0][2]} | N° {lista_produtos[0][3]}")
+    # print(type(lista_produtos))
+
+    area_produto = [
+        [sg.Text('Lista de Produtos')],
+        [sg.Listbox(lista_produtos, size=(100, 10), key='box')],
+        [sg.Button('Voltar', size=(16, 2)), sg.Button('Cadastrar Produtos', size=(16, 2))]
+    ]
+    pagina_4 = sg.Window('Área dos Produtos', area_produto, size=(380, 260), element_justification='center')
+    evento_pag_4, valor_pag_4 = pagina_4.read()
+
+    while True:
+        if evento_pag_4 == sg.WINDOW_CLOSED:
+            pagina_4.close()
+
+        if evento_pag_4 == 'Voltar':
+            pagina_4.close()
+            selecao()
+
+        if evento_pag_4 == 'Cadastrar Produtos':
+            pagina_4.close()
+            cadastro_produtos()
+
+
+def realizar_venda():
+    lista_produtos = back.consultar_produtos()
+    inicial = 1
+
+    venda = [
+        [sg.Text('Selecione o Produto')],
+        [sg.Listbox(lista_produtos, size=(40, 10), key='produto_selecionado')],
+        [sg.Text('Quantidade '), sg.Input(f'{inicial}', size=(4, 2), key='quantidade')],
+        [sg.Text('')],
+        [sg.Button('Cancelar', size=(14, 2)), sg.Button('Realizar Venda', size=(14, 2))]
+    ]
+
+    pagina_venda = sg.Window('Realizar Venda', venda, element_justification='center')
+    evento_pag_venda, valor_pag_venda = pagina_venda.read()
+
+    while True:
+
+        if evento_pag_venda == sg.WINDOW_CLOSED or evento_pag_venda == 'Cancelar':
+            pagina_venda.close()
+            area_vendas()
+
+        if evento_pag_venda == 'Realizar Venda':
+
+            data_venda = back.data_atual()
+            funcionario_venda = fun_cod
+            produto = valor_pag_venda['produto_selecionado'][0][0]
+            quantidade = int(valor_pag_venda['quantidade'])
+            print('__________________')
+            print(type(data_venda))
+            print(data_venda)
+            print(type(funcionario_venda))
+            print(funcionario_venda)
+            print(type(produto))
+            print(produto)
+            print(type(quantidade))
+            print(quantidade)
+            print('__________________')
+
+            back.realizar_venda(data_venda, funcionario_venda, produto, quantidade)
+
+            pagina_venda.close()
+            area_vendas()
+
+
+def cadastro_produtos():
+
+    cadastro_produto = [
+        [sg.Text('')],
+        [sg.Text('Item                   '), sg.InputText('', size=(50, 3), key='prod_nome')],
+        [sg.Text('Marca                '), sg.InputText('', size=(50, 3), key='prod_marca')],
+        [sg.Text('Tipo                   '), sg.InputText('', size=(50, 3), key='prod_tipo')],
+        [sg.Text('Preço de Custo  '), sg.InputText('', size=(10, 3), key='prod_custo')],
+        [sg.Text('Preço de Venda '), sg.InputText('', size=(10, 3), key='prod_venda')],
+        [sg.Text('Quantidade        '), sg.InputText('', size=(10, 3), key='prod_quantidade')],
+        [sg.Text('')],
+        [sg.Button('Cancelar', size=(14, 2)), sg.Button('Cadastrar', size=(14, 2))]
+    ]
+
+    pagina_5 = sg.Window('Cadastro de Produtos', cadastro_produto)
+    evento_pag_5, valor_pag_5 = pagina_5.read()
+
+    while True:
+
+        if evento_pag_5 == sg.WINDOW_CLOSED:
+            pagina_5.close()
+            area_produtos()
+
+        if evento_pag_5 == 'Cancelar':
+            pagina_5.close()
+            area_produtos()
+
+        if evento_pag_5 == 'Cadastrar':
+            prod_nome = valor_pag_5['prod_nome']
+            prod_marca = valor_pag_5['prod_marca']
+            prod_custo = valor_pag_5['prod_custo']
+            prod_venda = valor_pag_5['prod_venda']
+            prod_tipo = valor_pag_5['prod_tipo']
+            prod_quantidade = valor_pag_5['prod_quantidade']
+
+            if (prod_nome != '') or \
+                    (prod_marca != '') or \
+                    (prod_custo != '') or \
+                    (prod_venda != '') or \
+                    (prod_tipo != '') or \
+                    (prod_quantidade != ''):
+
+                back.cadastrar_produtos(prod_nome, prod_marca, prod_custo, prod_venda, prod_tipo, prod_quantidade)
+                pagina_5.close()
+                area_produtos()
+
+            else:
+                pagina_5.close()
+                area_produtos()
+
+
+front()
